dcc9b683885c1252fcd87d2a61bb2e614c0cd9db
Vishalofficail5 <vishal05.official@gmail.com>
2026-07-14 00:00:26 +0530

    DSA

--- src/PatternPrinting/Odd_Number_Triangle.java
+++ src/PatternPrinting/Odd_Number_Triangle.java
@@ -1,15 +1,14 @@
 package PatternPrinting;
 import java.util.Scanner;
-public class BinaryTriangle {
+public class Odd_Number_Triangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i=1; i<=n; i++){
+        int user_input = sc.nextInt();
+        for (int i=1; i<=user_input; i++){
+            int new_var=1;
             for (int j=1; j<=i; j++){
-                if((i+j)%2!=0)
-                    System.out.print("0 ");
-                else
-                    System.out.print("1 ");
+                System.out.print(new_var+" ");
+                new_var+=2;
             }
             System.out.println();
         }
