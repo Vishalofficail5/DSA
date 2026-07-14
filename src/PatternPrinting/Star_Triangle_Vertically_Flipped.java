7aeda3db5ee85b3d141cb8cf438c69eb4de39540
Vishalofficail5 <vishal05.official@gmail.com>
2026-07-14 23:31:50 +0530

    DSA

--- src/PatternPrinting/Star_Triangle_Vertically_Flipped.java
+++ src/PatternPrinting/Star_Triangle_Vertically_Flipped.java
@@ -4,10 +4,20 @@ public class Star_Triangle_Vertically_Flipped {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int user_input = sc.nextInt();
-        for (int i=1; i<=user_input; i++){
-            for (int j=1; j<=user_input; j++){
-                if ((i+j)>user_input) System.out.print("* ");
-                else System.out.print("  ");
+//        for (int i=1; i<=user_input; i++){
+//            for (int j=1; j<=user_input; j++){
+//                if ((i+j)>user_input) System.out.print("* ");
+//                else System.out.print("  ");
+//            }
+//            System.out.println();
+//        }
+        // ***New method***
+        for(int i=1; i<=user_input; i++){
+            for (int j=1; j<=user_input-i; j++){
+                System.out.print("  ");
+            }
+            for (int k=1; k<=i; k++){
+                System.out.print("* ");
             }
             System.out.println();
         }
