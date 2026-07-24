596b583c8eaafacd566fd8ea1342f761d8c22efa
Vishalofficail5 <vishal05.official@gmail.com>
2026-07-25 00:04:44 +0530

    feat: UPDATED ON 25-07-2026

--- src/PatternPrinting/Pyramid.java
+++ src/PatternPrinting/Pyramid.java
@@ -4,14 +4,27 @@ public class Pyramid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int user_input  = sc.nextInt();
+//        for (int i=1; i<=user_input; i++){
+//            for (int j=1; j<=user_input-i; j++){
+//                System.out.print("  ");
+//            }
+//            for (int k=1; k<=2*i-1; k++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        int nsp = user_input-1, nst = 1;
         for (int i=1; i<=user_input; i++){
-            for (int j=1; j<=user_input-i; j++){
+            for (int j=1; j<=nsp; j++){
                 System.out.print("  ");
             }
-            for (int k=1; k<=2*i-1; k++){
+            for (int k=1; k<=nst; k++){
                 System.out.print("* ");
             }
             System.out.println();
+            nsp--;
+            nst+=2;
         }
     }
 }
