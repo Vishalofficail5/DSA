87d544617cd8f4175dcce3936e0fd4f142181454
Vishalofficail5 <vishal05.official@gmail.com>
2026-07-29 00:21:56 +0530

    feat: UPDATED ON 29-07-2026

--- src/PatternPrinting/Number_Spiral.java
+++ src/PatternPrinting/Number_Spiral.java
@@ -4,16 +4,14 @@ public class Number_Spiral {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int user_input = sc.nextInt();
-        for (int i=1; i<=user_input; i++){
-            for (int j=1; j<=user_input; j++){
-                if (i<=j){
-                    System.out.print(i+" ");
-                }else if (j<=i) {
-                    System.out.print(j+" ");
-                }
+        for (int i=1; i<=2*user_input-1; i++){
+            for (int j=1; j<=2*user_input-1; j++){
+                int a = i,b = j;
+                if (i>user_input) a = 2*user_input-i;
+                if (j>user_input) b = 2*user_input-j;
+                System.out.print((Math.min(a,b))+" ");
             }
             System.out.println();
         }
     }
 }
-    
