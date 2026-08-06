8a7982bf9b4b90502fd09414a693e88bf4850c35
Vishalofficail5 <vishal05.official@gmail.com>
2026-08-07 00:11:10 +0530

    feat: Updated on 07-08-2026

--- src/Arrays/Negative_of_array.java
+++ src/Arrays/Negative_of_array.java
@@ -1,4 +1,19 @@
 package Arrays;
-
+import java.util.Scanner;
 public class Negative_of_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int array_size = sc.nextInt();
+        int[] arr = new int[array_size];
+        for (int i=0; i<array_size; i++){
+            System.out.print("Enter array: ");
+            arr[i] = sc.nextInt();
+        }
+        for (int j=0; j<array_size; j++){
+            if (arr[j] < 0){
+                System.out.print(arr[j]+" ");
+            }
+        }
+    }
 }
