f337ed56c6b6bf4bd247e1b91b5f901fa1e2a9c4
Vishalofficail5 <vishal05.official@gmail.com>
2026-09-20 16:00:18 +0530

    feat: Updated on 20-09-2026

--- src/Array/Max_array.java
+++ src/Array/Max_array.java
@@ -1,4 +1,19 @@
 package Array;
-
 public class Max_array {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,5,5,6,3,7,4,57,4,34,86};
+        int max = Integer.MIN_VALUE;
+        int smax = Integer.MIN_VALUE;
+
+        for (int i=0; i< arr.length; i++){
+            if (arr[i]>max)
+                max = arr[i];
+        }
+        for (int j=0; j<arr.length; j++){
+            if (arr[j]>smax && arr[j]!=max)
+                smax = arr[j];
+        }
+        System.out.println(smax);
+        System.out.print("This is max "+max);
+    }
 }
--- src/Array/Two_Sum.java
+++ src/Array/Two_Sum.java
@@ -1,4 +1,19 @@
 package Array;
-
+import java.util.Scanner;
 public class Two_Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Target: ");
+        int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+        int Target = sc.nextInt();
+        for (int i=0; i<Target; i++){
+            for (int j=1; j<Target; j++){
+                if (arr[i]+arr[j] == Target){
+                    System.out.print("True");
+                }
+                break;
+            }
+        }
+        System.out.print("false");
+    }
 }
